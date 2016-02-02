7ae5a2e23d0bb79d51989feb53e826215be58295
Your <ray_ios@163.com>
2016-02-02 16:48:55 +0800

    s

---
+++ Layer中的应用/ViewController.h
@@ -0,0 +1,19 @@
+//
+//  ViewController.h
+//  Layer中的应用
+//
+//  Created by Ray on 15/7/16.
+//  Copyright (c) 2015年 Ray. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
+
+static inline CGFloat DegreeToRadius(CGFloat degress){
+    return  degress*M_PI/180 ;
+}
